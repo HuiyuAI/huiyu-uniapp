7c659419e4caca722338e6bc621744b3a0119101
Naccl <i@naccl.top>
2023-07-05 20:26:42 +0800

    config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
    * @type {string}
    * @description 后端接口地址
    */
-  BASE_SERVER_URL: 'https://api.naccl.top/huiyugateway',
+  BASE_SERVER_URL: 'https://api.naccl.top/huiyu',
 };
